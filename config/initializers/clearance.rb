0e574be56a7c6d709b4274b4a074e1aad4110cf5
Alenxadre <alexandre.dosa@gmail.com>
2022-08-29 08:53:35 -0300

    Initial commit

---
+++ config/initializers/clearance.rb
@@ -0,0 +1,5 @@
+Clearance.configure do |config|
+  config.mailer_sender = ENV["MAILER_SENDER"]
+  config.rotate_csrf_on_sign_in = true
+  config.cookie_expiration = lambda { |cookies| 1.week.from_now.utc }
+end
