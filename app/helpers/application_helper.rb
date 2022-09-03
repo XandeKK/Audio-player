cca7be8d8a55b9fea8c0873055c95554dce63e98
Alenxadre <alexandre.dosa@gmail.com>
2022-09-03 07:37:42 -0300

    Add edit music

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
-  def form_label name
-    if @user.errors.messages[name].empty?
+  def form_label( name, model=@user)
+    if model.errors.messages[name].empty?
       "block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
     else
       "block mb-2 text-sm font-medium text-red-700 dark:text-red-500"
     end
   end
 
-  def input name
-    if @user.errors.messages[name].empty?
+  def input( name, model=@user)
+    if model.errors.messages[name].empty?
       "shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
     else
       "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-red-100 dark:border-red-400"  
@@ -25,7 +25,7 @@ module ApplicationHelper
     model.each do |author|
       json.append({
         "name": author.music.title,
-        "artist": "Artist Name",
+        "artist": author.user.artistic_name,
         "url": (url_for(author.music.music) if author.music.music.attached?),
         "cover_art_url": (url_for(author.music.image_cover) if author.music.image_cover.attached?)
       })
